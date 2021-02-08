0e0ab388ac38eaced1a24a7fb2dff2ae7ca2a543
Brandon Ting <brandon1221t@gmail.com>
2021-02-09 00:12:55 +0800

    feature: init line robot setting

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ var linebot = require('linebot');
 
 // 用於辨識Line Channel的資訊
 var bot = linebot({
-    channelId: '替換成你的CHANNEL_ID',
-    channelSecret: '替換成你的CHANNEL_SECRET',
-    channelAccessToken: '替換成你的CHANNEL_ACCESS_TOKEN'
+    channelId: '1655652906',
+    channelSecret: 'a7154b0695304eced5bbc98f99d00c05',
+    channelAccessToken: '+y0l+86z0nsIRniwUVnz7ajTkQKEUNDdwrm34IHeRdfPSCbvC14zsUnmBRZyX8ZEkd5iKPxev275knB30b3dVATMWmVldmmxAUVVB8xPvXVrVKe6Hu6k+ZgBMxtIDFdBsdYRI9YitbjS7Y73QD+3ewdB04t89/1O/w1cDnyilFU='
 });
 
 // 當有人傳送訊息給Bot時
 bot.on('message', function (event) {
     // event.message.text是使用者傳給bot的訊息
     // 使用event.reply(要回傳的訊息)方法可將訊息回傳給使用者
-    event.reply(event.message.text).then(function (data) {
+    const reply = `echo test: ${event.message.text}`;
+    event.reply(reply).then(function (data) {
         // 當訊息成功回傳後的處理
     }).catch(function (error) {
         // 當訊息回傳失敗後的處理
